c76bb6a5f6c8cb152b893e0acf68353b626a4c6f
marcomoriani <marco04mo@libero.it>
2022-07-08 15:24:58 +0200

    ss

--- Site/main2.js
+++ Site/main2.js
@@ -1,9 +1,8 @@
 function myFunction() {
     document.getElementById("txt-it-en").innerHTML = "Buongiorno! Oggi vi presento la mia famiglia. Io sono il padre, mi chiamo Gennaro Pirlo, ho trentasette anni, e lavoro come scrittore e giornalista da quando ne avevo venti. Mia moglie si chiama Antonella Totti, ha trentacinque anni ed è una splendida attrice di teatro. La nostra famiglia è composta anche da altre due persone, i nostri figli, Manuela che ha diciassette anni, e Marco che ha quindici anni, e poi c'è anche Tremendo, il cane che vive con noi da nove anni, ed è parte della famiglia. ";
-  }
+  };
 function myFunction2() {
     document.getElementById("txt-it-en").innerHTML = "Good morning!!! Today I would like to introduce you to my family. I am the father, my name is Gennaro Pirlo, I am thirty-seven years old, and I have been working as a writer and journalist since I was twenty. My wife's name is Antonella Totti, she is thirty-five years old and a wonderful theater actress. Our family also consists of two other people, our children, Manuela who is seventeen, and Marco who is fifteen, and then there is also Tremendous, the dog who has lived with us for nine years, and is part of the family.";
-  }
+  };
 
-  var anno= new Date().getFullYear();
-  document.getElementById('anno').innerHTML = anno;
+document.getElementById('anno').innerHTML = new Date().getFullYear();
